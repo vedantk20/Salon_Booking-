2de8c911981ddc803257a53b86b03b973bfe11d5
vedantk20 <vedantkundale20@gmail.com>
2026-06-24 00:15:09 +0530

    switch browser router with hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
